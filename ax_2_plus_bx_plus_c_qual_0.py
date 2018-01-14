58a4be6dabe4b7ca31c3bdb94fb937cfa0021538
douangtavanh <32052391+douangtavanh@users.noreply.github.com>
2018-01-14 13:21:45 +0700

    python added

---
+++ ax_2_plus_bx_plus_c_qual_0.py
@@ -0,0 +1,20 @@
+import math
+
+a = eval(input("Please enter a value: \n"))
+b = eval(input("Please enter b value: \n"))
+c = eval(input("Please enter c value: \n"))
+
+if (a == 0):
+    x = -c/b
+    print(x, "\n")
+else:
+    delta = math.pow(b, 2)-(4*a*c)
+    if (delta == 0):
+        x = -b/(2*a)
+        print(x, "\n")
+    elif (delta > 0):
+        x1 = (-b + math.sqrt(delta))/(2*a)
+        x2 = (-b - math.sqrt(delta))/(2*a)
+        print(x1, x2, "\n")
+    else:
+        print("Incorrect")
